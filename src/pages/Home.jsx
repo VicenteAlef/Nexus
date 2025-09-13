c57b19ab68cab28c5b225565abc9c6b3096a39eb
Vicente Silva <ildealef@outlook.com>
2025-09-13 16:54:41 -0300

    Section app created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -255,7 +255,72 @@ const Home = () => {
           />
         </div>
       </section>
-      <section id="app"></section>
+      <section id="app">
+        <div class="pt-30 px-5 md:px-[8%] lg:px-[15%] bg-[#0079bb] flex flex-col items-center grid grid-cols-1 md:grid-cols-2 text-white">
+          <div class="w-full flex flex-col items-center sm:items-start">
+            <div className="flex flex-col w-[280px] sm:w-full">
+              <h2 class="text-5xl lg:text-7xl text-white font-bold  mb-10 md:pb-[20px]">
+                Baixe o App
+              </h2>
+              <div>
+                <h3 class="text-2xl lg:text-4xl font-bold mb-5 self-center">
+                  Tudo na palma da mão
+                </h3>
+                <ul class="list-disc ml-5 lg:ml-10 md:text-2xl">
+                  <li>Solicite a segunda via de boletos</li>
+                  <li>Confira histórico de pagamentos</li>
+                  <li>Solicite atendimentos</li>
+                  <li>Assine documentos</li>
+                </ul>
+              </div>
+            </div>
+            <div class="flex flex-wrap py-10 lg:py-20 gap-5 justify-center md:justify-start">
+              <a
+                target="_blank"
+                href="https://play.google.com/store"
+                class="py-2 px-5 bg-white rounded-full text-black flex items-center gap-2 text-xl w-[180px] hover:scale-110 duration-500 ease-in-out"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-google-play"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M14.222 9.374c1.037-.61 1.037-2.137 0-2.748L11.528 5.04 8.32 8l3.207 2.96zm-3.595 2.116L7.583 8.68 1.03 14.73c.201 1.029 1.36 1.61 2.303 1.055zM1 13.396V2.603L6.846 8zM1.03 1.27l6.553 6.05 3.044-2.81L3.333.215C2.39-.341 1.231.24 1.03 1.27" />
+                </svg>
+                Play Store
+              </a>
+              <a
+                target="_blank"
+                href="https://apps.apple.com/br"
+                class="py-2 px-5 bg-white rounded-full text-black flex items-center gap-2 text-xl w-[180px] hover:scale-110 duration-500 ease-in-out"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-apple"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516s1.52.087 2.475-1.258.762-2.391.728-2.43m3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422s1.675-2.789 1.698-2.854-.597-.79-1.254-1.157a3.7 3.7 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56s.625 1.924 1.273 2.796c.576.984 1.34 1.667 1.659 1.899s1.219.386 1.843.067c.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758q.52-1.185.473-1.282" />
+                  <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516s1.52.087 2.475-1.258.762-2.391.728-2.43m3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422s1.675-2.789 1.698-2.854-.597-.79-1.254-1.157a3.7 3.7 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56s.625 1.924 1.273 2.796c.576.984 1.34 1.667 1.659 1.899s1.219.386 1.843.067c.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758q.52-1.185.473-1.282" />
+                </svg>
+                Apple Store
+              </a>
+            </div>
+          </div>
+          <div class="flex w-full items-center justify-center">
+            <img
+              src="assets/Adobe Express - file (1).png"
+              alt=""
+              class="w-full"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
